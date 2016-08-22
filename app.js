f62b8f444df7d4abe8e13bf03e9b888184bfe105
Aakash Singh Rathore <aakashrathore@email.arizona.edu>
2016-08-22 11:01:49 -0700

    now using vis for timeline and UA bootstrap for personalizarion

--- app.js
+++ app.js
@@ -1,127 +1,103 @@
-var app = angular.module('timeline', ['ngDraggable', 'ngRoute']);
-
-
-app.config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-      .when('/', {
-        controller:'myCtrl',
-        templateUrl: './Partials/submit.html'
-      })
-      .when('/result', {
-        controller:'myCtrl',
-        templateUrl:'./Partials/result.html'
-      });
-}]);
+var app = angular.module('timeline',[]);
 
 app.controller('myCtrl', ['$scope', function($scope){
-    $scope.view = true;
-    $scope.events = [{
-            yearsAgo: "13.8B",
-            name: "Big Bang",
-            field: "Astronomy"
-        },
-        {
-            yearsAgo: "13.8B",
-            name: "CMB Radiation",
-            field: "Astronomy"
-        },
-        {
-            yearsAgo: "13.5B",
-            name: "First Light",
-            field: "Astronomy"
-        },
-        {
-            yearsAgo: "4.5B",
-            name: "Earth and Sun Form", 
-            field: "Geology"
-        },
-        {
-            yearsAgo:"3.7B",
-            name: "First Life",
-            field: "Biology"
-        },
-        {
-            yearsAgo: "0.02M",
-            name: "Last Major Ice Age",
-            field: "Geology"
-        }
-    ];
 
-    $scope.droppedEvents = new Array(14);
-    $scope.maxMarks = $scope.events.length;
-    $scope.obtainedMarks = 0;
+    // DOM element where the Timeline will be attached
+    var container = document.getElementById('visualization');
 
-    for(var i=0; i<$scope.droppedEvents.length; i++){
-        $scope.droppedEvents[i] = {};
-        $scope.droppedEvents[i].name = ".";
-        $scope.droppedEvents[i].age = "0";
+    $scope.total = 6;
+    $scope.marks = 0;
+    $scope.display = true;
+    // Create a DataSet (allows two way data-binding)
+    var solution = new vis.DataSet([
+        {id: 0, content: 'Big Bang', start: 13800000000},
+        {id: 1, content: 'CMB Radiation', start: 13800000000},
+        {id: 2, content: 'First Light', start: 13500000000},
+        {id: 3, content: 'AGN Peak', start: 10000000000},
+        {id: 4, content: 'Earth Forms', start: 4500000000},
+        {id: 5, content: 'Hominids', start: 2000000}
+    ]);
+    $scope.data = [{id: 0, content: 'Big Bang', start: 13800000000, className:'btn btn-info'},
+        {id: 1, content: 'CMB Radiation', start: 0, className:'btn btn-info'},
+        {id: 2, content: 'First Light', start: 0, className:"btn btn-info"},
+        {id: 3, content: 'AGN Peak', start: 10000000000, className:"btn btn-info"},
+        {id: 4, content: 'Earth Forms', start: 4500000000, className:"btn btn-warning"},
+        {id: 5, content: 'Hominids', start: 0, className:"btn btn-danger"}];
+
+    // var items = new vis.DataSet([
+    // 	{id: 0, content: 'Big Bang', start: 13800000000, className:'btn btn-info'},
+    // 	{id: 1, content: 'CMB Radiation', start: 0, className:'btn btn-info'},
+    // 	{id: 2, content: 'First Light', start: 0, className:"btn btn-info"},
+    // 	{id: 3, content: 'AGN Peak', start: 10000000000, className:"btn btn-info"},
+    // 	{id: 4, content: 'Earth Forms', start: 4500000000, className:"btn btn-warning"},
+    // 	{id: 5, content: 'Hominids', start: 0, className:"btn btn-danger"}
+    // ]);
+
+    var datasetOptions = {};
+    var items = new vis.DataSet(datasetOptions);
+
+    $scope.add = function(index){
+        console.log(new Date(new Number($scope.data[index].start)).getTime());
+        $scope.data[index].start = new Date(new Number($scope.data[index].start)).getTime();
+        console.log($scope.data[index]);
+        items.add($scope.data[index]);
+        $scope.data.splice(index, 1);
     }
-
-    $scope.droppedEvents[0].age = "15B";
-    $scope.droppedEvents[1].age = "13.8B";
-    $scope.droppedEvents[2].age = "13.8B";
-    $scope.droppedEvents[3].age = "13.5B";
-    $scope.droppedEvents[4].age = "12B";
-    $scope.droppedEvents[5].age = "11.5B";
-    $scope.droppedEvents[6].age = "11B";
-    $scope.droppedEvents[7].age = "10B";
-    $scope.droppedEvents[8].age = "8B";
-    $scope.droppedEvents[9].age = "6B";
-    $scope.droppedEvents[10].age = "4.5B";
-    $scope.droppedEvents[11].age = "3.7B";
-    $scope.droppedEvents[12].age = "100M";
-    $scope.droppedEvents[13].age = "0.02M";
-
-
-
-    $scope.onDragComplete=function(data,evt){
-        console.log("Here");
-       console.log("drag success, data:", data);
-    };
-    $scope.onDragComplete2=function(data,evt, index){
-        //var index = $scope.droppedObjects1.indexOf(data);
-        // console.log("here");
-        // //var index = $scope.droppedEvents.indexOf(data);
-        // console.log(index);
-        // if(index == -1)
-        //     console.log("Oops! Not Found");
-        // else{
-        //     $scope.droppedEvents[index].name = ".";
-        //     $scope.droppedEvents[index].field = "0";
-        //     console.log($scope.droppedEvents);
-        // }
-    };
-    $scope.onDropComplete=function(data,evt, index){
-        console.log("drop success, data:", data);
-        if(index == -1)
-            console.log("Negative index");
-        else{
-            //$scope.droppedEvents.splice(index, 0, data);
-            //console.log("Position: " + index);
-            $scope.droppedEvents[index].name = data.name;
-            //console.log($scope.droppedEvents);
-            $scope.droppedEvents[index].field = data.field;
-            //$scope.$apply();
-        }
-    };
-
-    //Fix this. Inefficient AF!
-    $scope.checkAnswers = function(){
-        console.log($scope.droppedEvents);
-        for(var i=0; i<$scope.droppedEvents.length; i++){
-            for(var j=0; j<$scope.events.length; j++){
-                if($scope.droppedEvents[i].name == $scope.events[j].name){
-                    if($scope.droppedEvents[i].age == $scope.events[j].yearsAgo)
-                        $scope.obtainedMarks++;
-                    //alert("here");
-                    break;
-                }
+    // Configuration for the Timeline
+    var options = {
+        format:{
+            minorLabels: {
+                millisecond:'x',
+                second:     'x',
+                minute:     'x',
+                hour:       'x',
+                weekday:    'x',
+                day:        'x',
+                month:      'x',
+                year:       'x'
+            },
+            majorLabels: {
+                millisecond:'',
+                second:     '',
+                minute:     '',
+                hour:       '',
+                weekday:    '',
+                day:        '',
+                month:      '',
+                year:       ''
             }
+        },
+        showMajorLabels: false,
+        start: 0,
+        end: 14000000000,
+        min:0,
+        max:14000000000,
+        rtl: true,
+        zoomMin:10,
+        itemsAlwaysDraggable: true,
+        minHeight: '300px',
+        editable: {
+            updateTime: true
         }
-        console.log("you scored " + $scope.obtainedMarks);
-        $scope.view = !$scope.view;
-        //$window.location.href = '/result'
-        //$scope.$apply(function() { $location.path("/result"); });
-    }
+    };
 
+    // Create a Timeline
+    var timeline = new vis.Timeline(container, items, options);
+    timeline.getItemRange();
+
+// add event listener
+// 	timeline.on('click', function (properties) {
+// 		console.log(properties);
+// 	});
+    $scope.submit = function(){
+        //console.log("Here");
+        $scope.display = !$scope.display;
+        //console.log(items._data[0]);
+        for(var i=0; i<6; i++)
+        {
+            if((new Date(items._data[i].start).getTime()) == new Date(solution._data[i].start).getTime())
+                $scope.marks++;
+        }
+
+    }
 }]);
